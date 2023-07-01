f55336af407f22f636ee65e12857d497f2ad48b3
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-01 21:31:03 +0300

    Import example component CSS in docs pages

--- docs/src/components/Markup/index.tsx
+++ docs/src/components/Markup/index.tsx
@@ -3,6 +3,9 @@
 import { type Doc } from 'contentlayer/generated';
 import { getMDXComponent } from 'next-contentlayer/hooks';
 
+// TODO: Find better way to import CSS per docs page
+import '@theomer77/omer-ui-styles/Button.css';
+
 import styles from './index.module.css';
 
 const Markup = ({ doc }: { doc: Doc }) => {
