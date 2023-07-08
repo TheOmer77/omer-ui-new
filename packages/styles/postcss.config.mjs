eba765849d2f74753f7a1a456dfa72fa71440fd7
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-08 20:34:50 +0300

    Add `postcss-hover-media-feature` to PostCSS config

--- packages/styles/postcss.config.mjs
+++ packages/styles/postcss.config.mjs
@@ -1,11 +1,17 @@
 //@ts-check
 
 import cssnano from 'cssnano';
+import hoverMedia from 'postcss-hover-media-feature';
 import nested from 'postcss-nested';
 import presetEnv from 'postcss-preset-env';
 
 const config = {
-  plugins: [nested(), presetEnv(), cssnano({ preset: 'default' })],
+  plugins: [
+    nested(),
+    hoverMedia(),
+    presetEnv(),
+    cssnano({ preset: 'default' }),
+  ],
 };
 
 export default config;
